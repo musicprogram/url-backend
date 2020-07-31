97127624546f7edbc75cf97106f7852f89a98a2a
musicprogram <jahenao17@misena.edu.co>
2020-07-31 13:45:07 -0500

    url rack kors

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module BackLinks
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000','https://elated-heisenberg-6e8a4a.netlify.app/'
+        origins 'https://elated-heisenberg-6e8a4a.netlify.app/'
         resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
       end
     end
