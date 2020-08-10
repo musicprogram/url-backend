39ce5aa1d8186d096ef781648ed83ecb20536c81
musicprogram <jahenao17@misena.edu.co>
2020-08-10 09:43:28 -0500

    add links rack cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://elated-heisenberg-6e8a4a.netlify.app/'
+    origins 'https://elated-heisenberg-6e8a4a.netlify.app/'
 
     resource '*',
              headers: :any,
