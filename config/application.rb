ee2ae8d02f00a18220c296779301e51f60517009
musicprogram <jahenao17@misena.edu.co>
2020-08-14 12:17:28 -0500

    configurando rack-cors

--- config/application.rb
+++ config/application.rb
@@ -24,12 +24,13 @@ module BackLinks
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0 and config.autoloader = :classic
 
-    config.middleware.insert_before "ActionDispatch::Static", Rack::Cors do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://elated-heisenberg-6e8a4a.netlify.app/'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/initializers/cors.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.config.middleware.insert_before "ActionDispatch::Static", Rack::Cors do
-  allow do
-    origins 'https://elated-heisenberg-6e8a4a.netlify.app/'
-
-    resource '*',
-             headers: :any,
-             methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
-end
